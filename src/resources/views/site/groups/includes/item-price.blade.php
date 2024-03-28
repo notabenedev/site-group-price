6dc504ee93dab0fc3923c1067e29493ef8621fbe
GIS <dev@gis4biz.ru>
2024-03-28 12:37:35 +0300

    id price__list

--- src/resources/views/site/groups/includes/item-price.blade.php
+++ src/resources/views/site/groups/includes/item-price.blade.php
@@ -1,5 +1,6 @@
-<div class="row price__list{{ isset($margin) ? " mx-0": "" }}{{ ($loop->iteration % 2 == 0) ? "" : " price__list-iteration" }}">
-    <div class="col-12 col-sm-7 col-md-8 col-lg-9 price__list-title" id="{{ $price["slug"]}}">
+<div class="row price__list{{ isset($margin) ? " mx-0": "" }}{{ ($loop->iteration % 2 == 0) ? "" : " price__list-iteration" }}"
+     id="{{ $price["slug"]}}">
+    <div class="col-12 col-sm-7 col-md-8 col-lg-9 price__list-title">
         {{ $price["title"] }}
         @isset($price["description"])
             <div class="price__list-description">
